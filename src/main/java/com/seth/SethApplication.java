706016035a2a5e50a97994d2f3f3a1dc6cc4d008
GBASKI-CODDED <rasaqagbalaya@gmail.com>
2016-08-07 09:15:50 +0100

    first comit

---
+++ src/main/java/com/seth/SethApplication.java
@@ -0,0 +1,12 @@
+package com.seth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SethApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SethApplication.class, args);
+	}
+}
